15421b29377d6a6c0bfc37669bda3c9dce60c796
sam cross <sam.cross@gmx.com>
2023-07-21 23:58:37 +0000

    made app and index

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import userRoute from './controler/Users.js'
-import choreRoute from './controler/chores.js'
-import logRoute from './controler/logs.js'
-import indexRoute from './controler/index.js'
-
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 
@@ -18,9 +12,4 @@ app.use('/logs',logRoute)
 
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-export default app;
+export default app
