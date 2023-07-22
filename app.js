5fdf4054dd17c82119f584689330f9e1fbe1ed7b
sam cross <sam.cross@gmx.com>
2023-07-22 09:41:43 +0000

    test

--- app.js
+++ app.js
@@ -7,15 +7,17 @@ import indexRoute from './controler/index.js'
 
 const app = express();
 console.log('starting  up');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.set('view engine', 'ejs');
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.set('view engine', 'ejs');
 
 
-app.use('/',indexRoute)
-app.use('/users',userRoute)
-app.use('/chores',choreRoute)
-app.use('/logs',logRoute)
-
+// app.use('/',indexRoute)
+// app.use('/users',userRoute)
+// app.use('/chores',choreRoute)
+// app.use('/logs',logRoute)
+app.get('/',(req,res)=>{
+    res.send('home')
+  })
 
 app.use(express.static('public'));
 
