1ffbb21a0cd0098c50d8e5cfa926efda6b460343
sam cross <sam.cross@gmx.com>
2023-07-22 09:59:59 +0000

    test

--- api/index.js
+++ api/index.js
@@ -1,15 +1,19 @@
 import express from 'express';
+import indexRoute from 'indexRoute';
+import userRoute from 'userRoute';
+import choreRoute from 'choreRoute';
+import logRoute from 'logRoute';
 
 const app = express();
 console.log('starting  up');
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
 
 
-// app.use('/',indexRoute)
-// app.use('/users',userRoute)
-// app.use('/chores',choreRoute)
-// app.use('/logs',logRoute)
+app.use('/',indexRoute)
+app.use('/users',userRoute)
+app.use('/chores',choreRoute)
+app.use('/logs',logRoute)
 app.get('/api',(req,res)=>{
     res.send('api')
   })
